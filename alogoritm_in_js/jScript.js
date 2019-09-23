459798213a99749be18d41e3bd54a3f5e1b651b8
mohamadas1992 <54312963+mohamadas1992@users.noreply.github.com>
2019-09-23 14:26:55 +0330

    Run

--- alogoritm_in_js/jScript.js
+++ alogoritm_in_js/jScript.js
@@ -52,26 +52,7 @@ if (s===n){
 }else{
 	console.log("nist");
 }
-//////////////////////////////adade aval*******************************jkar nmikoneh
-var n = Number(prompt("enter number"));
-for (var i = 2; i < n; i++) {
-	var r=n%i;
-	if (r===0){
-		console.log("Aval nist");
-	}console.log("Aval AST");
-}
 
-var n = Number(prompt("enter number"));
-var i = 2;
-while (i<n){
-	var r=n%i;
-	if (r===0){
-		console.log("Aval nist");
-	}else{
-		i=i+1;
-	}
-	console.log("Aval AST");
-}
 
 ///////////////////////////factoril
 var n = Number(prompt("enter number"));
@@ -80,32 +61,7 @@ for (var i = 1; i<=n; i++) {
 	p=p*i;
 }
 console.log(p);
-////////////////////////tedade argam***************
-var n = Number(prompt("enter number"));
-var s=0;
-var r=0;
-var w = 0;
-for(var i=0;n>0;i++){
-	 r=n%10;
-		s=s+r;
-		w++;
-		n=n/10;
-}
-console.log("maghmohe"+s);
-console.log("argme"+w);
 
-var n = Number(prompt("enter number"));
-var s=0;
-var r=0;
-var w = 0;
-do{
-	 r=n%10;
-		s=s+r;
-		w++;
-		n=n/10;
-}while(n>0)
-console.log("maghmohe"+s);
-console.log("argme"+w);
 /////mabna 10 bea 2
 var n = parseInt(prompt("enter number"));
 var temp=[];
