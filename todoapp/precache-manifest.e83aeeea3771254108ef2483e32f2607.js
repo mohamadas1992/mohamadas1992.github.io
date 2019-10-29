61c2e15873a309698e74c84603ad99c1890df1b5
mohamadas1992 <54312963+mohamadas1992@users.noreply.github.com>
2019-10-29 11:25:39 +0330

    ToDo

---
+++ todoapp/precache-manifest.e83aeeea3771254108ef2483e32f2607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1e4a76961afca191dd4e2a50c93912",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "500ce45269ac74648fd7",
+    "url": "/todoapp/static/css/main.e1ee7ed7.chunk.css"
+  },
+  {
+    "revision": "0468c274d3fc29292846",
+    "url": "/todoapp/static/js/2.215645c9.chunk.js"
+  },
+  {
+    "revision": "500ce45269ac74648fd7",
+    "url": "/todoapp/static/js/main.408381bb.chunk.js"
+  },
+  {
+    "revision": "80f8f6c5b874c7fc3aeb",
+    "url": "/todoapp/static/js/runtime-main.e9ca35db.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todoapp/static/media/logo.25bf045c.svg"
+  }
+]);
