00cb21e0c60a3fc752fe3d5382a6221ed4c176f0
mohamadas1992 <54312963+mohamadas1992@users.noreply.github.com>
2019-11-03 11:45:58 +0330

    add first-shop

---
+++ first-shop/precache-manifest.30408742c572acff59cb5cb9109be74d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2cb814e2524f5ae666b0657e327d8d8",
+    "url": "/first-shop/index.html"
+  },
+  {
+    "revision": "1edf635feeeb9380e8f8",
+    "url": "/first-shop/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "095411afb100a1523410",
+    "url": "/first-shop/static/css/main.35cc7078.chunk.css"
+  },
+  {
+    "revision": "1edf635feeeb9380e8f8",
+    "url": "/first-shop/static/js/2.3beb3b8a.chunk.js"
+  },
+  {
+    "revision": "095411afb100a1523410",
+    "url": "/first-shop/static/js/main.9ddee7b9.chunk.js"
+  },
+  {
+    "revision": "370be875f3ecf899ce3f",
+    "url": "/first-shop/static/js/runtime~main.448563e3.js"
+  }
+]);
